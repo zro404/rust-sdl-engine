b4c27b4ded3dceeee11527a1c67c2bba1284c997
Harish G <dev.harish3124@gmail.com>
2024-12-18 18:53:54 +0530

    Setup Texture loading, Add CharacterBody

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,103 @@
-use std::time::Duration;
+use std::{path::Path, time::Duration};
 
-use sdl2::{event::Event, keyboard::Keycode, pixels::Color};
+use sdl2::{
+    event::Event,
+    image::{InitFlag, LoadTexture},
+    keyboard::Keycode,
+    pixels::Color,
+    rect::{Point, Rect},
+    render::{Texture, WindowCanvas},
+};
 
-extern crate sdl2;
+// #[derive(Debug)]
+struct CharacterBody<'a> {
+    texture: Texture<'a>,
+    sprite: Rect,
+    position: Point,
+    speed: i32,
+}
 
-fn main() {
-    let ctx = sdl2::init().unwrap();
-    let video_subsystem = ctx.video().unwrap();
+fn render(canvas: &mut WindowCanvas, body: &CharacterBody) -> Result<(), String> {
+    let (width, height) = canvas.output_size()?;
+    // Treat the center of the screen as the (0, 0) coordinate
+    let screen_position = body.position + Point::new(width as i32 / 2, height as i32 / 2);
+    let screen_rect = Rect::from_center(screen_position, body.sprite.width(), body.sprite.height());
+
+    canvas.copy(&body.texture, body.sprite, screen_rect)?;
+
+    Ok(())
+}
+
+fn main() -> Result<(), String> {
+    let ctx = sdl2::init()?;
+    let video_subsystem = ctx.video()?;
+    let _image_ctx = sdl2::image::init(InitFlag::PNG | InitFlag::JPG);
 
     let window = video_subsystem
         .window("rust sdl engine", 1280, 720)
         .position_centered()
         .build()
-        .unwrap();
+        .expect("Could not initialize video subsystem");
 
-    let mut canvas = window.into_canvas().build().unwrap();
+    let mut canvas = window
+        .into_canvas()
+        .build()
+        .expect("Could not create canvas");
 
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
+    let texture_creator = canvas.texture_creator();
+    let mut event_pump = ctx.event_pump()?;
+
+    canvas.set_draw_color(Color::RGB(136, 192, 208));
     canvas.clear();
     canvas.present();
 
-    let mut event_pump = ctx.event_pump().unwrap();
-    let mut i = 0;
+    let mut player = CharacterBody {
+        texture: texture_creator.load_texture(&Path::new("assets/reaper.png"))?,
+        position: Point::new(0, 0),
+        sprite: Rect::new(0, 0, 26, 36),
+        speed: 10,
+    };
 
     'running: loop {
-        i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        canvas.clear();
         for event in event_pump.poll_iter() {
             match event {
                 Event::Quit { .. }
                 | Event::KeyDown {
                     keycode: Some(Keycode::Escape),
                     ..
-                } => break 'running,
+                } => break 'running Ok(()),
+                Event::KeyDown {
+                    keycode: Some(Keycode::Left),
+                    ..
+                } => {
+                    player.position = player.position.offset(-player.speed, 0);
+                }
+                Event::KeyDown {
+                    keycode: Some(Keycode::Right),
+                    ..
+                } => {
+                    player.position = player.position.offset(player.speed, 0);
+                }
+                Event::KeyDown {
+                    keycode: Some(Keycode::Up),
+                    ..
+                } => {
+                    player.position = player.position.offset(0, -player.speed);
+                }
+                Event::KeyDown {
+                    keycode: Some(Keycode::Down),
+                    ..
+                } => {
+                    player.position = player.position.offset(0, player.speed);
+                }
                 _ => {}
             }
         }
+
         // Game Loop
+        canvas.set_draw_color(Color::RGB(136, 192, 208));
+        canvas.clear();
+        render(&mut canvas, &player)?;
 
         canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
